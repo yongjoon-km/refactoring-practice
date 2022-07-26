bf7b4619cde1f411a072683fec77deaf99fc37bb
yongjoon <penubokim@gmail.com>
2022-07-26 22:23:27 +0900

    Refactor first version of converter

--- yaml-to-json/convert.js
+++ yaml-to-json/convert.js
@@ -1,53 +1,59 @@
 function convert(yamlText) {
-
   const yamlTextLines = yamlText.split('\n')
 
   let result = "{"
-  let previousIndent = 0;
-  const defaultPadding = 2;
+  let prevRowIndent = 0;
+  const BASE_PADDING = 2;
   for (let textLine of yamlTextLines) {
     if (!textLine) {
-      let previousIndentString = ""
-      for (let i = 0; i < previousIndent; i++) {
-        previousIndentString += " "
-      }
-      result += `\n${previousIndentString}}\n`
+      result += `${separator('')}${padding(prevRowIndent)}}\n`
       break;
     }
     const [key, value] = textLine.split(':')
-    let indent = 0;
-    for (let i = 0; i < key.length; i++) {
-      if (key[i] === " ") {
-        indent++;
-      } else {
-        break;
-      }
-    }
-    let keyText = key.slice(indent)
-    let indentString = ""
-    for (let i = 0; i < indent + defaultPadding; i++) {
-      indentString += " "
-    }
-
-    if (!!value) {
-      if (indent == previousIndent) {
-        result += ',\n'
-      } else {
-        result += '\n'
-      }
-      let valueText = value.trim()
-      if (valueText[0] === "'") {
-        valueText = value.split("'")[1]
-        valueText = `"${valueText}"`
-      }
-      result += `${indentString}"${keyText}": ${valueText}`
-    } else {
-      result += `\n${indentString}"${key}": {`
-    }
-    previousIndent = indent
+    result += `${separator(key)}${padding(indentOf(key) + BASE_PADDING)}"${jsonText(key)}": `
+    result += `${!!value ? jsonText(value) : '{'}`
+    prevRowIndent = indentOf(key)
   }
   result += "}\n"
   return result
+
+  function separator(key) {
+    if (indentOf(key) === prevRowIndent) {
+      return ',\n'
+    } else {
+      return '\n'
+    }
+  }
+}
+
+
+function jsonText(text) {
+  let result = text.trim()
+  if (result[0] === "'") {
+    result = text.split("'")[1]
+    result = `"${result}"`
+  }
+  return result;
+}
+
+function indentOf(text) {
+  let result = ""
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === " ") {
+      result++;
+    } else {
+      break;
+    }
+  }
+  return result
+}
+
+function padding(paddingCount) {
+  let result = ""
+  for (let i = 0; i < paddingCount; i++) {
+    result += " "
+  }
+  return result;
 }
 
 module.exports = convert
