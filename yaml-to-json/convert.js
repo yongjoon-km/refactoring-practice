7b38a39efa3986fb4f100927eaae06251a1853c2
yongjoon <penubokim@gmail.com>
2022-07-26 21:41:44 +0900

    First version of converter

--- yaml-to-json/convert.js
+++ yaml-to-json/convert.js
@@ -1,10 +1,51 @@
-function convert(yamlString) {
-  let result = "{\n"
+function convert(yamlText) {
 
-  result += `  "application": {\n`
-  result += `    "host": "www.example.com",\n`,
-  result += `    "port": 443\n`,
-  result += `  }\n`,
+  const yamlTextLines = yamlText.split('\n')
+
+  let result = "{"
+  let previousIndent = 0;
+  const defaultPadding = 2;
+  for (let textLine of yamlTextLines) {
+    if (!textLine) {
+      let previousIndentString = ""
+      for (let i = 0; i < previousIndent; i++) {
+        previousIndentString += " "
+      }
+      result += `\n${previousIndentString}}\n`
+      break;
+    }
+    const [key, value] = textLine.split(':')
+    let indent = 0;
+    for (let i = 0; i < key.length; i++) {
+      if (key[i] === " ") {
+        indent++;
+      } else {
+        break;
+      }
+    }
+    let keyText = key.slice(indent)
+    let indentString = ""
+    for (let i = 0; i < indent + defaultPadding; i++) {
+      indentString += " "
+    }
+
+    if (!!value) {
+      if (indent == previousIndent) {
+        result += ',\n'
+      } else {
+        result += '\n'
+      }
+      let valueText = value.trim()
+      if (valueText[0] === "'") {
+        valueText = value.split("'")[1]
+        valueText = `"${valueText}"`
+      }
+      result += `${indentString}"${keyText}": ${valueText}`
+    } else {
+      result += `\n${indentString}"${key}": {`
+    }
+    previousIndent = indent
+  }
   result += "}\n"
   return result
 }
--- yaml-to-json/convert.test.js
+++ yaml-to-json/convert.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const convert = require('./convert');
-const yamlText = fs.readFileSync(`${__dirname}/test1.yaml`);
+const yamlText = fs.readFileSync(`${__dirname}/test1.yaml`, 'utf-8');
 
 test('it should read yaml', () => {
   expect(convert(yamlText)).toBe(
